bff34f11e80ab22f6aa1566d034e87138201b9a8
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-12-15 21:45:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -70,7 +70,7 @@ module.exports = {
     AFKMSG: process.env.AFK_MESSAGE === undefined ? 'default' : process.env.AFK_MESSAGE,
     WEL_GIF: process.env.WEL_GIF === undefined ? 'https://i.imgur.com/nErXUGj.mp4' : process.env.WEL_GIF,
     GIF_BYE: process.env.GIF_BYE === undefined ? 'https://i.imgur.com/Z1jCYGN.mp4' : process.env.GIF_BYE,
-    ALLEMOJI: process.env.CMD_LIST === undefined ? 'siya/🃏/🌈/⚠️/📝' : process.env.CMD_LIST,
+    ALLEMOJI: process.env.CMD_LIST === undefined ? 'Helena🎗️/💝/✨/📝' : process.env.CMD_LIST,
     HANDLERS: process.env.HANDLERS === undefined ? '^[.!;]' : process.env.HANDLERS,
     TAGPLK: process.env.TAG_HEADER === undefined ? 'Note this' : process.env.TAG_HEADER,
     SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
