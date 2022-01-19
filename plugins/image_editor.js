b6aef9f2e52f98dd21c75d76e7214fea4b46bf6f
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-01-19 06:59:05 +0530

    Update image_editor.js

--- plugins/image_editor.js
+++ plugins/image_editor.js
@@ -1,7 +1,12 @@
-
-
-
-
+let e = require('../events');
+let { MessageType, Mimetype } = require('@adiwajshing/baileys');
+let w = require('../config');
+let v = w.SESSION
+let v1 = w.SUPPORT2
+let i = require('amalser');
+let a = w.WORKTYPE == 'public' ? false : true;
+let ffmpeg = require('fluent-ffmpeg');
+let fs = require('fs');
 e.addCommand({pattern: 'wanted ?(.*)', fromMe: a, desc:'Edits photo to a wanted effect'}, (async (m, match) => { 
 if (!m.reply_message.image) return await m.sendMessage("_Reply to an image!_")
 var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
