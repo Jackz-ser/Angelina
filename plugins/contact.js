9be8e484e5d564065398f5d3cf8ed5d1610a8e08
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-01-30 10:41:26 +0530

    Dont update

---
+++ plugins/contact.js
@@ -0,0 +1,17 @@
+const Ktb = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+
+//coded by saidali
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:Amalser [OWNER]\n' // full name
+            + 'ORG:Amalser;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=918075641889:+91 7994765065\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid,{displayname: "ANGELINA [OWNER]", vcard: vcard}, MessageType.contact)
+}))
