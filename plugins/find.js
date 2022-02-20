949ec4cf699dfc6f0977fd937085426583d477c5
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-02-20 18:44:55 +0530

    Update find.js

--- plugins/find.js
+++ plugins/find.js
@@ -3,10 +3,10 @@ var {MessageType,Mimetype} = require('@adiwajshing/baileys')
 const c = require('../config')
 var v = c.SESSION
 var fm = c.WORKTYPE == 'public' ? false : true
-e.addCommand({pattern: 'find ?(.*)', fromMe: fm}, (async (m, match) => {    
+e.addCommand({pattern: 'find', fromMe: fm}, (async (m, match) => {    
 if (!m.reply_message.text && !m.reply_message.video && !m.reply_message.sticker && !m.reply_message.image) {
 var q = await m.client.downloadAndSaveMediaMessage({key: { remoteJid: m.reply_message.jid,id: m.reply_message.id}, message: m.reply_message.data.quotedMessage});
-var k = c.find_key
+var k = c.FIND_KEY
 var r = await f.query.music(q,k,v)
 if (r.result) {
 let msg =  '_Title:_ *' + r.result.title + '* \n_Album:_ *' + r.result.album+'* \n_Artist:_ *' + r.result.artist+ '* \n_Label:_ *' + r.result.label+'* \n_Release date:_ *' + r.result.release_date + '* \n_Song link:_ ' + 'https://www.youtube.com/results?search_query='+r.result.title.split(' ').join('+')
