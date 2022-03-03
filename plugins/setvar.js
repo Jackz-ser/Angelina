37d8d5c608c3ed410cfba8b60c5f2fd33f39e8e1
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-03-03 12:45:40 +0530

    All setvar in setvarcmnds

--- plugins/setvar.js
+++ plugins/setvar.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'setvar', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'setvarcmnds', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
     
